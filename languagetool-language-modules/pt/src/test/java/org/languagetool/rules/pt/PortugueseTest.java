ac79bfe33dcaf605e5cfaa0ff1537206ac6ac3d3
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2024-11-15 11:05:58 +0400

    Made Portuguese languages a singletons

--- languagetool-language-modules/pt/src/test/java/org/languagetool/rules/pt/PortugueseTest.java
+++ languagetool-language-modules/pt/src/test/java/org/languagetool/rules/pt/PortugueseTest.java
@@ -32,7 +32,7 @@ public class PortugueseTest extends LanguageSpecificTest {
   public void testLanguage() throws IOException {
     // NOTE: this text needs to be kept in sync with config.ts -> DEMO_TEXTS:
     String s = "Cole aqui seu texto...ou verifique esta texto, afim de revelar alguns dos dos problemas que o LanguageTool consegue detectar. Isto tal vez permita corrigir os seus erro. Nós prometo ajudá-lo. para testar a grafia e as regrs do antigo) Acordo Ortográfico,, verifique o mesmo texto mesmo texto em Português de Angola ou Português do Moçambique e faça a analise dos resultados.. Nossa equipe anuncia a versão 4.5, que será lançada sexta-feira, 26 de março de 2019.";
-    PortugalPortuguese ptPT = new PortugalPortuguese();
+    PortugalPortuguese ptPT = PortugalPortuguese.getInstance();
     testDemoText(ptPT, s,
       Arrays.asList("POSSESSIVE_WITHOUT_ARTICLE", "SPACE_AFTER_PUNCTUATION", "GENERAL_GENDER_AGREEMENT_ERRORS", "AFIM_DE", "PORTUGUESE_WORD_REPEAT_RULE",
               "PT_AGREEMENT_REPLACE_DETECTAR", "TAL_VEZ", "GENERAL_NUMBER_AGREEMENT_ERRORS", "GENERAL_VERB_AGREEMENT_ERRORS", "UPPERCASE_SENTENCE_START",
@@ -40,7 +40,7 @@ public class PortugueseTest extends LanguageSpecificTest {
               "PARONYM_ANALISE_363", "DOUBLE_PUNCTUATION", "POSSESSIVE_WITHOUT_ARTICLE", "EQUIPES", "PT_DATE_WEEKDAY")
     );
     runTests(ptPT, null, "õș");
-    BrazilianPortuguese ptBR = new BrazilianPortuguese();
+    BrazilianPortuguese ptBR = BrazilianPortuguese.getInstance();
     String s2 = "Cole aqui seu texto...ou verifique esta texto, afim de revelar alguns dos dos problemas que o LanguageTool consegue detectar. Isto tal vez permita corrigir os seus erro. Nós prometo ajudá-lo. para testar a grafia e as regrs do antigo) Acordo Ortográfico,, verifique o mesmo texto mesmo texto em Português de Angola ou Português do Moçambique e faça a analise dos resultados.. Nossa equipe anuncia a versão 4.5, que será lançada sexta-feira, 26 de março de 2019.";
     testDemoText(ptBR, s2,
       Arrays.asList("SPACE_AFTER_PUNCTUATION", "GENERAL_GENDER_AGREEMENT_ERRORS", "AFIM_DE", "PORTUGUESE_WORD_REPEAT_RULE", "TAL_VEZ",
